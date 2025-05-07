b0c6b5965dc0236c6595c38c6292eee592d94b22
Chaser <32584944+Chaser-0@users.noreply.github.com>
2025-05-07 17:29:44 +0200

    Added some dummy data

--- ui/src/dummyOffers.ts
+++ ui/src/dummyOffers.ts
@@ -1,21 +1,37 @@
 type TBaseOffer = {
-  id: string;
   name: string;
   cost: number;
+  address: string;
 };
 
+type TAddonOffer = TBaseOffer & {
+  supplierPercent: number;
+  supplierAddress: string;
+}
+
 type TTravelOffer = TBaseOffer & {
-  addons: TBaseOffer[];
+  addons: TAddonOffer[];
 }
 
 
+// TODO Replace with real addresses
 export const exampleData: TTravelOffer[] = [
   {
-    id: 'awesome_offer_1',
-    name: 'Crete',
+    name: '1 Week on Crete',
     cost: 1.000356,
+    address: '0xdeadbeefdeadbeefdeadbeefdeadbeef',
     addons: [
-      {name: 'Breakfast', id: 'addon_1', cost: 0.00035}
+      {name: 'Breakfast', cost: 0.00035, address: '0xdeadbeefdeadbeefdeadbeefdeadbeef', supplierAddress: '0xdeadbeefdeadbeefdeadbeefdeadbeef', supplierPercent: 10},
+      {name: 'Spa day', cost: 0.149, address: '0xdeadbeefdeadbeefdeadbeefdeadbeef', supplierAddress: '0xdeadbeefdeadbeefdeadbeefdeadbeef', supplierPercent: 10}
+    ]
+  },
+  {
+    name: '1 Week on Mallorca',
+    cost: 1.000356,
+    address: '0xdeadbeefdeadbeefdeadbeefdeadbeef',
+    addons: [
+      {name: 'Breakfast', cost: 0.00035, address: '0xdeadbeefdeadbeefdeadbeefdeadbeef', supplierAddress: '0xdeadbeefdeadbeefdeadbeefdeadbeef', supplierPercent: 10},
+      {name: 'Spa day', cost: 0.149, address: '0xdeadbeefdeadbeefdeadbeefdeadbeef', supplierAddress: '0xdeadbeefdeadbeefdeadbeefdeadbeef', supplierPercent: 10}
     ]
   }
 ];

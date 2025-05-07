8f934acdfe353eae391e4d3440f6eef296d1914b
Chaser <32584944+Chaser-0@users.noreply.github.com>
2025-05-07 15:31:56 +0200

    Added tailwind to ui

---
+++ ui/src/layouts/MainLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink :to="{name: 'searchOffers'}">Search offers</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <div class="flex flex-col gap-2">
+    <h1>{{ headline }}</h1>
+    <main class="container mx-auto bg-gray-950 rounded-2xl">
+      <slot></slot>
+    </main>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  headline: string;
+}>();
+</script>

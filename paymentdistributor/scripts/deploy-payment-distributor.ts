1f6a8f60a3e98b082b8d135c735603988c5e5269
Mihai Hangan <m.hangan@netmatch.ro>
2025-05-08 12:37:38 +0300

    Extract generic method for deploying a smart contract and reuse it to deploy existing contracts

--- paymentdistributor/scripts/deploy-payment-distributor.ts
+++ paymentdistributor/scripts/deploy-payment-distributor.ts
@@ -1,19 +1,9 @@
-import { ethers } from "hardhat"
-
-// TODO: Make this a generic deploy script for any contract
+import { deployContract } from "./deploy-contract";
 
 async function main() {
-	// Creating an instance of Contract Factory. Think of it as making a blueprint. In line 9
-	// we are addressing this blueprint to deploy the actual contract.
-	const escrowContract = await ethers.getContractFactory("PaymentDistributor");
-	// Tokens use denomination in decimals, by default it is 18 (e.g. input of 1 * 10 ** 18 = 1 token)
-	// ethers.utils.parseEther is basically adding 18 decimals to the number you input.
-	const deployedContract = await escrowContract.deploy(/* constructor arguments if any */);
-	await deployedContract.deployed();
+	const contractName = "PaymentDistributor"; // Name of the contract to deploy
 
-	console.log(
-		"PaymentDistributor contract deployed to:", deployedContract.address
-	)
+	deployContract(contractName, null);
 }
 
 main()

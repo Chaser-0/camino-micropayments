6155179d15771b3190334ed181ba4849a3e7cab2
Chaser <32584944+Chaser-0@users.noreply.github.com>
2025-05-08 16:53:42 +0200

    Fanzyness > 9000

--- ui/src/components/OfferListEntry.vue
+++ ui/src/components/OfferListEntry.vue
@@ -1,24 +1,43 @@
 <template>
-  <li class="group relative w-full h-64 rounded-md overflow-hidden">
-    <img :src="offer.heroImage" class="absolute top-0 left-0 w-full h-full object-cover group-hover:brightness-125 transition-[filter] duration-500">
-    <div class="absolute bottom-0 left-0 w-full h-3/4 bg-gradient-to-t from-black/80 to-transparent"></div>
+  <li class="group relative w-full h-64 rounded-md">
+    <div ref="antroposEl" class="atropos w-full h-full">
+      <div class="atropos-scale">
+        <div class="atropos-rotate">
+          <div class="atropos-inner">
+            <img data-atropos-offset="0" :src="offer.heroImage" class="absolute top-0 left-0 w-full h-full object-cover group-hover:brightness-125 transition-[filter] duration-500 rounded-md">
+            <div data-atropos-offset="0" class="absolute bottom-0 left-0 w-full h-3/4 bg-gradient-to-t from-black/80 to-transparent"></div>
 
-    <RouterLink
-      class="absolute top-0 left-0 w-full h-full p-2 | flex flex-col gap-2 justify-end"
-      :to="{name: 'offerDetails', params: {id: id}}"
-      >
-      <span class="text-white text-3xl font-medium">{{ offer.name }}</span>
-      <span class="text-gray-300">{{ offer.shortDescription }}</span>
-      <span class="text-gray-200">Starting from <span class="font-bold">{{ offer.price }} CAM</span></span>
-    </RouterLink>
+            <RouterLink
+              data-atropos-offset="3"
+              class="absolute top-0 left-0 w-full h-full p-2 | flex flex-col gap-2 justify-end"
+              :to="{name: 'offerDetails', params: {id: id}}"
+              >
+              <span class="text-white text-3xl font-medium">{{ offer.name }}</span>
+              <span class="text-gray-300">{{ offer.shortDescription }}</span>
+              <span class="text-gray-200">Starting from <span class="font-bold">{{ offer.price }} CAM</span></span>
+            </RouterLink>
+          </div>
+        </div>
+      </div>
+    </div>
   </li>
 </template>
 
 <script setup lang="ts">
 import type { TTravelOffer } from '@/dummyOffers.ts';
+import Atropos from 'atropos';
+import { onMounted, ref } from 'vue';
 
 defineProps<{
   offer: TTravelOffer;
   id: string;
 }>();
+
+const antroposEl = ref<HTMLElement>();
+
+onMounted(() => {
+  const myAtropos = Atropos({
+    el: antroposEl.value,
+  });
+});
 </script>
